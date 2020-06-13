3a1ce5bcc3dafaee957bdb3c4e9db78d37c1511a
kchia <kchia87@gmail.com>
2020-06-13 17:15:02 -0400

    make edits

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 # TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
-resource "aws_instance" "Udacity_T2" {
+resource "aws_instance" "Udacity T2" {
   count         = 4
   ami           = var.ami
   instance_type = "t2.micro"
@@ -18,7 +18,7 @@ resource "aws_instance" "Udacity_T2" {
 # TODO: provision 2 m4.large EC2 instances named Udacity M4
 
 # Code commented out to facilitate deletion of the M4 instances
-# resource "aws_instance" "Udacity_M4" {
+# resource "aws_instance" "Udacity M4" {
 #   count         = 2
 #   ami           = var.ami
 #   instance_type = "m4.large"
